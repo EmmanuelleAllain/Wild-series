bc8aec0ed193061f7be7831e22c32e5c7f8dc334
EmmanuelleAllain <e.allain13@gmail.com>
2022-05-27 14:11:18 +0200

    quest11

--- src/DataFixtures/ProgramFixtures.php
+++ src/DataFixtures/ProgramFixtures.php
@@ -9,40 +9,24 @@ use Doctrine\Persistence\ObjectManager;
 
 class ProgramFixtures extends Fixture implements DependentFixtureInterface
 {
+    public const PROGRAMS = [
+        ['title' => 'Peppa Pig', 'synopsis' => 'Peppa vit avec son frère Georges et ses parents dans une maison sur la colline.', 'category' => 'Animation'],
+        ['title' => 'Desperate Housewives', 'synopsis' => 'C\'est l\'histoire de dames qui s\'ennuient dans leur maison.', 'category' => 'Aventure'],
+        ['title' => 'Peaky Blinders', 'synopsis' => 'C\'est l\'histoire d\'un monsieur, un peu fou mais sympa dans le fond, et de ses frères qui l\aident à réaliser ses rêves', 'category' => 'Action'],
+        ['title' => 'How I met your mother', 'synopsis' => 'Ted raconte à ses enfants comment il a rencontré sa femme, et ça lui prend 9 saisons.', 'category' => 'Humour'],
+        ['title' => 'Breaking Bad', 'synopsis' => 'Un père de famille sympa à la base se lance dans le commerce de drogue et devient un peu moins sympa.', 'category' => 'Action'],
+    ];
+ 
     public function load(ObjectManager $manager): void
     {
+        foreach (self::PROGRAMS as $key => $programName)
         $program = new Program();
         $program
-            ->setTitle('Peppa Pig')
-            ->setSynopsis('Peppa vit avec son frère Georges et ses parents dans une maison sur la colline.')
-            ->setCategory($this->getReference('category_Animation'));
+            ->setTitle($programName['title'])
+            ->setSynopsis($programName['synopsis'])
+            ->setCategory($this->getReference('category_' . $programName['category']));
+        $this->addReference('program_' . $key, $program);
         $manager->persist($program);
-
-        $program1 = new Program();
-        $program1->setTitle('Desperate Housewives');
-        $program1->setSynopsis('C\'est l\'histoire de dames qui s\'ennuient dans leur maison.');
-        $program1->setCategory($this->getReference('category_Action'));
-        $manager->persist($program1);
-
-        $program2 = new Program();
-        $program2->setTitle('Peaky Blinders');
-        $program2->setSynopsis('C\'est l\'histoire d\'un monsieur, un peu fou mais sympa dans le fond, et de ses frères qui l\aident à réaliser ses rêves');
-        $program2->setCategory($this->getReference('category_Action'));
-        $manager->persist($program2);
-
-        $program3 = new Program();
-        $program3->setTitle('How I met your mother');
-        $program3->setSynopsis('Ted raconte à ses enfants comment il a rencontré sa femme, et ça lui prend 9 saisons.');
-        $program3->setCategory($this->getReference('category_Humour'));
-        $manager->persist($program3);
-
-        $program4 = new Program();
-        $program4->setTitle('Breaking bad');
-        $program4->setSynopsis('Un père de famille sympa à la base se lance dans le commerce de drogue et devient un peu moins sympa.');
-        $program4->setCategory($this->getReference('category_Action'));
-        $manager->persist($program4);
-
-        $this->addReference('program_' . $program->title, $program);
 
         $manager->flush();
     }
